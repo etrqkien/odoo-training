8d0f1ff5e01ad523d0f6841a7a743a7f0b35a954
thientd <tranthien126@gmail.com>
2018-11-29 10:35:55 +0700

    Add book source code

---
+++ B05246_Code/Chapter 10/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'To-Do Reports',
+    'description': 'Reports on to-do tasks.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_kanban'],
+    'data': [
+        'reports/todo_report.xml',
+        'reports/todo_model_report.xml',
+        'security/ir.model.access.csv',
+        ],
+}

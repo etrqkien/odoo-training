b43b17618ca0508844594a9e704016769870d84d
vutien129 <anhs2em4rever@gmail.com>
2018-12-07 14:57:45 +0700

    Reaqured to false

--- B05246_Code/Chapter 02/todo_model.py
+++ B05246_Code/Chapter 02/todo_model.py
@@ -6,7 +6,7 @@ class TodoTask(models.Model):
     _name = 'todo.task'
     _description = 'To-do Task'
 
-    name = fields.Char('Description', required=True)
+    name = fields.Char('Description', required=False)
     is_done = fields.Boolean('Done?')
     active = fields.Boolean('Active?', default=False)
 

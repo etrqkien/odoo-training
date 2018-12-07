1bb04130ed12673cb9491f6f234beca1cd3611a3
vuvantien1 <anhs2em4rever@gmail.com>
2018-12-07 13:10:37 +0700

    add comment

---
+++ vuvantien/Chapter 02/__manifest__.py
@@ -0,0 +1,7 @@
+{'name': 'chap2-vvt',
+ 'data': [
+     'security/ir.model.access.csv',
+     'security/todo_access_rules.xml',
+     'views/todo_menu.xml',
+     'views/todo_view.xml'],
+ 'application': True}

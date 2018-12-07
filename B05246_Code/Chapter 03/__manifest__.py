0e2eb54eb7f47bad34b9d54e119486371e1b3df6
vutien129 <anhs2em4rever@gmail.com>
2018-12-07 15:32:00 +0700

    tìm hiểu chap3

--- B05246_Code/Chapter 03/__manifest__.py
+++ B05246_Code/Chapter 03/__manifest__.py
@@ -1,14 +1,13 @@
 {
-    'name': 'Multiuser To-Do',
+    'name': 'chap3vvt',
     'description': 'Extend To-Do Tasks for multiuser',
     'author': 'Daniel Reis',
-    'depends': ['todo_app', 'mail'],
+    'depends': ['Chapter 02', 'mail'],
     'data': [
         'views/todo_task.xml',
         'security/todo_access_rules.xml'],
     'demo': [
 	    # Ch04 Data files
         'data/todo.task.csv',
-        'data/todo_data.xml',  
-    ],
-}
+        'data/todo_data.xml'],
+    'application': True}

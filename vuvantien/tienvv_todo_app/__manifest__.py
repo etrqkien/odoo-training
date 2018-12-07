bc1aaf03459e527539f2da977863ee32f0d6e48a
vutien129 <anhs2em4rever@gmail.com>
2018-12-07 17:29:48 +0700

    tìm hiểu chuong 2

---
+++ vuvantien/tienvv_todo_app/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'todo_app',
+    'data': [
+        'security/ir.model.access.csv',
+        'security/todo_access_rules.xml',
+        'views/todo_menu.xml',
+        'views/todo_view.xml'],
+    'application': True}
+

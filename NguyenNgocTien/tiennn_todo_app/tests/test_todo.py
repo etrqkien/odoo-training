1ea397e69970f3e6532fe2d86f450a3f867a787a
ngoctienentrust <trang78952@gmail.com>
2018-12-09 15:29:22 -0800

    sua file 12

--- NguyenNgocTien/tiennn_todo_app/tests/test_todo.py
+++ NguyenNgocTien/tiennn_todo_app/tests/test_todo.py
@@ -5,24 +5,22 @@ from odoo.tests.common import TransactionCase
 
 class TestTodo(TransactionCase):
 
-    def setUp(self, *args, **kwargs):
-        result = super(TestTodo, self).setUp(*args, **kwargs)
-        user_demo = self.env.ref('base.user_demo')
-        self.env= self.env(user=user_demo)
-        return result
-
     def test_create(self):
         "Create a simple Todo"
-        Todo = self.env['todo.task']
-        task = Todo.create({'name': 'Test Task'})
+        task = self.env['todo.task'].create({'name': 'Test Task'})
         self.assertEqual(task.is_done, False)
 
         # Test Toggle Done
         task.do_toggle_done()
         self.assertTrue(task.is_done)
         # Test Clear Done
-        Todo.do_clear_done()
         self.assertFalse(task.active)
+
+    def setUp(self, *args, **kwargs):
+        result = super(TestTodo, self).setUp(*args, **kwargs)
+        user_demo = self.env.ref('base.user_demo')
+        self.env = self.env(user=user_demo)
+        return result
 
     def test_record_rule(self):
         "Test per user record rules"

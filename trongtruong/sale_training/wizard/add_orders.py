8812f9cf55fd47e329262caf7d6816657cf9f704
trongtruong9xnd <trongtruong9xnd@gmail.com>
2019-10-26 10:06:07 +0700

    sửa   US #6034 US #6035

--- trongtruong/sale_training/wizard/add_orders.py
+++ trongtruong/sale_training/wizard/add_orders.py
@@ -4,13 +4,12 @@ from odoo import models, fields, api
 class AddTagToOrders(models.TransientModel):
     _name = 'add.orders'
 
-    order = fields.Many2many(comodel_name="sale.order", string="Order",
-                             domain=[('state', 'not in', ('draft', 'sent', 'cancel'))])
+    order_ids = fields.Many2many(comodel_name="sale.order", string="Order",
+                                 domain=[('state', 'not in', ('draft', 'sent', 'cancel'))])
 
     @api.multi
     def add_orders(self):
-        if self.order:
-            for i in self.order:
-                partner_id = self.env.context.get('active_id', False)
-                partner = self.env['sale.order.tag'].browse(partner_id)
-                i.tag_ids += partner
+        for order in self.order_ids:
+            partner_id = self.env.context.get('active_id', False)
+            partner = self.env['sale.order.tag'].browse(partner_id)
+            order.tag_ids += partner

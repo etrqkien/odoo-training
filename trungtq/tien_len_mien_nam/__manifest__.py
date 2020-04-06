79f27c592751ca2a6bd15075e37cc727ca67631b
TrungTrinhQuang <49512949+TrungTrinhQuang@users.noreply.github.com>
2020-04-06 18:01:58 +0700

    Đã edit tên field vs gán giá trị cho biến

---
+++ trungtq/tien_len_mien_nam/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Lịch sử chơi bài',
+    'version': '1.1',
+    'category': '',
+    'summary': 'Điểm chơi bài',
+    'description': """Tính điểm chơi bài theo từng round""",
+    'category': 'Application',
+    'website': 'https://odoo.vn',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/match_making_view.xml', ],
+    'demo': [],
+
+}

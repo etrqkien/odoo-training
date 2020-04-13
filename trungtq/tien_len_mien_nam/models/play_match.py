547dc24e4b99574cd46cd48401d0c0a620c03de6
TrungTrinhQuang <49512949+TrungTrinhQuang@users.noreply.github.com>
2020-04-13 08:43:01 +0700

    Ẩn filed trạng thái và sửa hàm tính điểm

--- trungtq/tien_len_mien_nam/models/play_match.py
+++ trungtq/tien_len_mien_nam/models/play_match.py
@@ -6,19 +6,22 @@ class PlayMatch(models.Model):
 
     play_zoom_id = fields.Many2one(comodel_name='play.room')
     round = fields.Char(string='Round', required=True)
-    player1_score_match = fields.Integer('Score player 1')
-    player2_score_match = fields.Integer('Score player 2')
-    player3_score_match = fields.Integer('Score player 3')
-    player4_score_match = fields.Integer('Score player 4')
-    check_point = fields.Boolean(default=True)
+    player1_score_match = fields.Integer('Score player 1', required=True)
+    player2_score_match = fields.Integer('Score player 2', required=True)
+    player3_score_match = fields.Integer('Score player 3', required=True)
+    player4_score_match = fields.Integer('Score player 4', required=True)
+    check_point = fields.Boolean(default=True, compute='_compute_check_point', store=True)
 
     @api.onchange('player3_score_match', 'player1_score_match', 'player2_score_match')
     def _onchange_player4_score(self):
-        self.player4_score_match = -(self.player1_score_match + self.player2_score_match + self.player3_score_match)
+        if self.player1_score_match and self.player2_score_match and self.player3_score_match:
+            if not self.player4_score_match:
+                self.player4_score_match -= self.player1_score_match + self.player2_score_match + self.player3_score_match
 
-    @api.onchange('player3_score_match', 'player1_score_match', 'player2_score_match', 'player4_score_match')
-    def _onchange_check_point(self):
-        if self.player1_score_match + self.player2_score_match + self.player3_score_match + self.player4_score_match == 0:
+    @api.depends('player3_score_match', 'player1_score_match', 'player2_score_match', 'player4_score_match')
+    def _compute_check_point(self):
+        checkpoint = self.player1_score_match + self.player2_score_match + self.player3_score_match + self.player4_score_match
+        if checkpoint == 0:
             self.check_point = True
         else:
             self.check_point = False

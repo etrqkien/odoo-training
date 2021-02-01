cf7cec13150742ad4e91e589b31a8189339ce950
tuanleanh <hocontapdanh@gmail.com>
2021-02-01 16:45:15 +0700

    fix

--- tuanle/tienlenmiennam/wizard/wizard.py
+++ tuanle/tienlenmiennam/wizard/wizard.py
@@ -10,17 +10,25 @@ class PlayWizard(models.TransientModel):
     player3_score = fields.Char()
     player4_score = fields.Char()
     play_room_id = fields.Many2one('play.room', string='play_room_id',default=_default_head_branch)
-    def check_valdationError(self):
-        try:
-            int(self.player1_score)
-            int(self.player2_score)
-            int(self.player3_score)
-            int(self.player4_score)
-        except ValueError:
-            raise UserError('bạn đang nhập chữ mời bạn nhâp số')
+    
     @api.onchange('player4_score','player3_score','player2_score','player1_score')
     def _onchange_player_score(self):
-        self.check_valdationError()
+        try:
+            int(self.player1_score)
+        except ValueError:
+            self.player1_score = None
+        try:
+            int(self.player2_score)
+        except ValueError:
+            self.player2_score = None
+        try:
+            int(self.player3_score)
+        except ValueError:
+            self.player3_score = None
+        try:
+            int(self.player4_score)
+        except ValueError:
+            self.player4_score = None
         if self.player1_score == False and self.player2_score != False and self.player3_score != False and self.player4_score != False:
             self.player1_score = 0 - (int(self.player2_score) + int(self.player3_score) + int(self.player4_score))
 
@@ -32,9 +40,10 @@ class PlayWizard(models.TransientModel):
 
         elif self.player4_score == False and self.player2_score != False and self.player3_score != False and self.player1_score != False:
             self.player4_score = 0 - (int(self.player1_score) + int(self.player2_score) + int(self.player3_score))
+
     def multi_update(self):
-        self.check_valdationError()
         if int(self.player1_score) + int(self.player2_score) + int(self.player3_score) + int(self.player4_score) != 0:
+
             raise ValidationError("mời bạn nhập lại kết quả" )
         else:
             vals = {

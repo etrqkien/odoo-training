c24b5a6cf81e45289bed7f3a9eda856ca6cd580d
SirCryALot <dangminhtuan000@gmail.com>
2024-06-03 16:43:59 +0700

    commit code

---
+++ dangminhtuan/odoo_training/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'training odoo',
+    'version': '1.0.0',
+    'category': 'training odoo',
+    'author': 'SirCryALot',
+    'sequence': -100,
+    'summary': 'training odoo',
+    'description': """
+training odoo
+    """,
+    'depends': ['sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {},
+}
---
+++ dangminhtuan/odoo_training/models/res_partner.py
@@ -0,0 +1,16 @@
+from odoo.exceptions import ValidationError
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    x_identity_card = fields.Char(string='Chứng minh thư')
+
+    @api.constrains('x_identity_card')
+    def check_identity_card(self):
+        for record in self:
+            if record.x_identity_card and self.search(
+                    [('x_identity_card', '=', record.x_identity_card), ('id', '!=', record.id)]):
+                raise ValidationError("Chứng minh thư đã tồn tại.")

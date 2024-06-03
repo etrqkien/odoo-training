5ed9aca6fe6dfb14e16bf5fe05f2a8560db7cada
SirCryALot <dangminhtuan000@gmail.com>
2024-06-03 09:53:05 +0700

    commit code

--- dangminhtuan/om_hospital/models/__init__.py
+++ dangminhtuan/om_hospital/models/__init__.py
@@ -1,4 +1,4 @@
 from . import patient_tag
 from . import res_country_state_district
 from . import res_partner
-from . import order
+from . import sale_order
--- dangminhtuan/om_hospital/models/sale_order.py
+++ dangminhtuan/om_hospital/models/sale_order.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-class Order(models.Model):
+class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     tag_ids = fields.Many2many('patient.tag',relation='tag_order_rel', column2='order_ids', column1='tag_ids' , string='Tags')

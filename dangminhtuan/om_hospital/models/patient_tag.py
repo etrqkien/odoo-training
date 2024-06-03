a24f1fcb3f0f5945cdb913d99670f7f6bfbdbc91
SirCryALot <dangminhtuan000@gmail.com>
2024-06-03 10:13:25 +0700

    commit code

--- dangminhtuan/om_hospital/models/patient_tag.py
+++ dangminhtuan/om_hospital/models/patient_tag.py
@@ -6,7 +6,5 @@ class PatientTag(models.Model):
     _description = 'Patient Tag'
 
     name = fields.Char(string='Name', required=True)
-    active = fields.Boolean(string='Active', default=True)
-    color = fields.Integer(string='Color')
     sale_order_ids = fields.Many2many('sale.order', relation='tag_order_rel', column1='order_ids', column2='tag_ids',
                                       string='Sale Orders')

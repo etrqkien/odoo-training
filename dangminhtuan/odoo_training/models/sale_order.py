eaf93b202777c473e23b4ac87be007d548a19cdf
SirCryALot <dangminhtuan000@gmail.com>
2024-06-04 11:04:00 +0700

    commit code

--- dangminhtuan/odoo_training/models/sale_order.py
+++ dangminhtuan/odoo_training/models/sale_order.py
@@ -7,4 +7,11 @@ class Order(models.Model):
     tag_ids = fields.Many2many('order.tag', relation='tag_order_rel', column2='order_id', column1='tag_id',
                                string='Tags')
 
-
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        docs = self.browse(docids)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'sale.order',
+            'docs': docs,
+        }

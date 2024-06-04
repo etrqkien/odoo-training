6beac57eeb61a5cbe02d254528a1d4db35259007
SirCryALot <dangminhtuan000@gmail.com>
2024-06-04 09:25:13 +0700

    commit code

---
+++ dangminhtuan/odoo_training/models/order_tag.py
@@ -0,0 +1,11 @@
+from odoo import fields, models, api
+
+
+class OrderTag(models.Model):
+    _name = 'order.tag'
+    _description = 'Order Tag'
+
+    name = fields.Char(string='Name', required=True)
+    color = fields.Integer(string='Color')
+    sale_order_ids = fields.Many2many('sale.order', relation='tag_order_rel', column1='order_ids', column2='tag_ids',
+                                      string='Sale Orders')
---
+++ dangminhtuan/odoo_training/models/sale_order.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class Order(models.Model):
+    _inherit = 'sale.order'
+
+    tag_ids = fields.Many2many('order.tag', relation='tag_order_rel', column2='order_ids', column1='tag_ids',
+                               string='Tags')
